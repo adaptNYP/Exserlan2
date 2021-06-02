bbcab856e6a1448336b479154c78ff03551a82cb
adaptNYP <58022041+adaptNYP@users.noreply.github.com>
2021-06-03 06:45:34 +0800

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1623,7 +1623,6 @@ String.prototype.unEscapeSpecialChars = function() {
 
 function displayFullText(evt) {
 	textAreaModal.style.display = 'block';
-	console.log($(evt).data().fulltext.unEscapeSpecialChars());
 	$('#myTextAreaModal').find('p').html($(evt).data().fulltext.unEscapeSpecialChars());
 }
 
